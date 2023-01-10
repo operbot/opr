f13d6ee45ae59d154afdff7385a6ade434766879
B.H.J. Thate <thatebhj@gmail.com>
2023-01-10 21:38:26 +0100

    up version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def uploadlist(dir):
 
 setup(
     name="opr",
-    version="9",
+    version="10",
     author="Bart Thate",
     author_email="operbot100@gmail.com",
     url="http://github.com/operbot/opr",
